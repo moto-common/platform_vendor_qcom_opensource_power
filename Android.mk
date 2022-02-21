ee4d6d2e09a18d55805e4f271d21e235237fd44f
electimon <electimon@gmail.com>
2022-02-21 20:56:06 +0000

    power: Add 4350 impl with basic touch boosting
    Signed-off-by: electimon <electimon@gmail.com>

--- Android.mk
+++ Android.mk
@@ -114,6 +114,10 @@ ifeq ($(call is-board-platform-in-list,trinket), true)
 LOCAL_SRC_FILES += power-6125.c
 endif
 
+ifeq ($(call is-board-platform-in-list,holi), true)
+LOCAL_SRC_FILES += power-4350.c
+endif
+
 endif # End of board specific list
 
 ifneq ($(TARGET_POWERHAL_MODE_EXT),)
